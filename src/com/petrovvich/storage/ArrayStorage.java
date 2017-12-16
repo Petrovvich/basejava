f1b6bcb8e77d44ea8551bcc6aa10a230a0540078
petrovvich <alexromantrue@gmail.com>
2017-12-17 03:18:11 +0700

    homework HW1 for basejava project

--- src/com/petrovvich/storage/ArrayStorage.java
+++ src/com/petrovvich/storage/ArrayStorage.java
@@ -7,26 +7,57 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume result = null;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                if (storage[i].toString().equals(uuid)) {
+                    result = storage[i];
+                }
+            }
+        }
+        return result;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                if (storage[i].toString().equals(uuid)) {
+                    storage[i] = null;
+                    break;
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = storage;
+        return result;
     }
 
     int size() {
-        return 0;
+        return storage.length;
+    }
+
+
+    public static void main(String[] args) {
+
     }
 }
