029414e10f3f422f490881332e0cbc86d4018c3e
petrovvich <alexromantrue@gmail.com>
2018-01-13 00:29:03 +0700

    optional homework HW4 for basejava project

---
+++ src/com/petrovvich/webapp/MainReflection.java
@@ -0,0 +1,26 @@
+package com.petrovvich.webapp;
+
+import com.petrovvich.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println();
+        System.out.println(field.get(r));
+        field.set(r, "new uuid");
+        System.out.println();
+        System.out.println(r);
+
+        //Optional homework for HW4
+        Method method = r.getClass().getDeclaredMethods()[1];
+        System.out.println(method.invoke(r));
+
+    }
+}
