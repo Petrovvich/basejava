ee310bc5087589173913b7882e695feb95446126
petrovvich <alexromantrue@gmail.com>
2018-01-27 17:08:06 +0700

    HW5 put in order AbstractArrayStorage

--- src/com/petrovvich/webapp/storage/AbstractArrayStorage.java
+++ src/com/petrovvich/webapp/storage/AbstractArrayStorage.java
@@ -14,54 +14,50 @@ public abstract class AbstractArrayStorage extends AbstractStorage {
     protected int sizeOfArray = 0;
 
     @Override
-    public void clear() {
-        Arrays.fill(storage, 0, sizeOfArray, null);
-        sizeOfArray = 0;
+    protected Resume getResumeFromStorage(Object searchIndex) {
+        return storage[(Integer) searchIndex];
     }
 
-    protected void insertElementInStorage(Object searchIndex, Resume resume) {
-        insertElement(resume, (Integer) searchIndex);
-        sizeOfArray++;
+    @Override
+    protected boolean checkIndex(Object searchIndex) {
+        return (Integer) searchIndex < 0;
     }
 
-    protected abstract void insertElement(Resume r, int index);
-
-    protected abstract void deleteElement(int index);
-
+    @Override
     protected void deleteResumeFromStorage(Object searchIndex) {
         deleteElement((Integer) searchIndex);
         storage[sizeOfArray - 1] = null;
         sizeOfArray--;
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    @Override
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, sizeOfArray);
-    }
+    protected abstract void deleteElement(int index);
 
     @Override
-
-
-    protected Resume getResumeFromStorage(Object searchIndex) {
-        return storage[(Integer) searchIndex];
+    protected void insertElementInStorage(Object searchIndex, Resume resume) {
+        insertElement(resume, (Integer) searchIndex);
+        sizeOfArray++;
     }
 
-    protected boolean checkIndex(Object searchIndex) {
-        if ((Integer) searchIndex < 0) {
-            return false;
-        }
-        return true;
+    @Override
+    protected void updateElementInStorage(Object searchIndex, Resume resume) {
+        insertElement(resume, (Integer) searchIndex);
     }
+
+    protected abstract void insertElement(Resume r, int index);
 
     @Override
     public int size() {
         return sizeOfArray;
     }
 
-    protected void updateElementInStorage(Object searchIndex, Resume resume) {
-        insertElement(resume, (Integer) searchIndex);
+    @Override
+    public void clear() {
+        Arrays.fill(storage, 0, sizeOfArray, null);
+        sizeOfArray = 0;
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, sizeOfArray);
     }
 }
