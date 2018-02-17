a8e8fdc6b31540978a1125abd89e449c5aa277ff
petrovvich <alexromantrue@gmail.com>
2018-02-17 14:30:28 +0700

    HW7 added enum and testsingleton

---
+++ src/com/petrovvich/webapp/TestSingleton.java
@@ -0,0 +1,15 @@
+package com.petrovvich.webapp;
+
+public class TestSingleton {
+    private static TestSingleton instance;
+
+    public static TestSingleton getInstance() {
+        if (instance == null) {
+            instance = new TestSingleton();
+        }
+        return instance;
+    }
+
+    private TestSingleton() {
+    }
+}
---
+++ src/com/petrovvich/webapp/model/SectionType.java
@@ -0,0 +1,26 @@
+package com.petrovvich.webapp.model;
+
+public enum SectionType {
+    PERSONAL("Личные качества"),
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+    SectionType() {}
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
