3a5258905ce67f8bb28ba3ef4d4847daf784cc14
petrovvich <alexromantrue@gmail.com>
2018-02-17 14:13:55 +0700

    HW7 added generics to all storages

--- src/com/petrovvich/webapp/storage/AbstractStorage.java
+++ src/com/petrovvich/webapp/storage/AbstractStorage.java
@@ -7,13 +7,17 @@ import com.petrovvich.webapp.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
+
+    private static final Logger LOGGER = Logger.getLogger(AbstractStorage.class.getName());
 
     protected static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid).thenComparing(Resume::getFullname);
 
     @Override
     public Resume get(String uuid) {
+        LOGGER.info("Get resume with uuid " + uuid);
         SK searchIndex = getSearchIndex(uuid);
         boolean validateIndex = checkIndex(searchIndex);
         if (validateIndex) {
@@ -30,9 +34,11 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public void delete(String uuid) {
+        LOGGER.info("Delete resume with uuid " + uuid);
         SK searchIndex = getSearchIndex(uuid);
         boolean validateIndex = checkIndex(searchIndex);
         if (validateIndex) {
+            LOGGER.warning("Can't delete resume " + uuid + ". Resume is not in Storage!");
             throw new NotExistStorageException(uuid);
         } else {
             deleteResumeFromStorage(searchIndex);
@@ -43,9 +49,11 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public void save(Resume r) {
+        LOGGER.info("Save resume " + r);
         SK searchIndex = getSearchIndex(r.getUuid());
         boolean validateIndex = checkIndex(searchIndex);
         if (!validateIndex) {
+            LOGGER.warning("Can't save resume " + r + ". Resume is already in Storage!");
             throw new ExistStorageException(r.getUuid());
         } else {
             insertElementInStorage(searchIndex, r);
@@ -55,9 +63,11 @@ public abstract class AbstractStorage<SK> implements Storage {
     protected abstract void insertElementInStorage(SK searchIndex, Resume resume);
 
     public void update(Resume resume) {
+        LOGGER.info("Update resume " + resume);
         SK searchIndex = getSearchIndex(resume.getUuid());
         boolean validateIndex = checkIndex(searchIndex);
         if (validateIndex) {
+            LOGGER.warning("Can't update resume " + resume + ". Resume is not in Storage!");
             throw new ExistStorageException(resume.getUuid());
         }
         updateElementInStorage(searchIndex, resume);
@@ -66,6 +76,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     protected abstract void updateElementInStorage(SK searchIndex, Resume resume);
 
     public List<Resume> getAllSorted() {
+        LOGGER.info("Get all sorted");
         List<Resume> result = getListedResumes();
         result.sort(RESUME_COMPARATOR);
         return result;
