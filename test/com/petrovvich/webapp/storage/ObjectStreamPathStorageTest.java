7f3a18ec45f0b0eaec23d1cf263051b93104ef16
petrovvich <alexromantrue@gmail.com>
2018-03-17 18:51:25 +0700

    HW9 fixed tests for ObjectStreamPathStorage

--- test/com/petrovvich/webapp/storage/ObjectStreamPathStorageTest.java
+++ test/com/petrovvich/webapp/storage/ObjectStreamPathStorageTest.java
@@ -1,9 +1,10 @@
 package com.petrovvich.webapp.storage;
 
+import com.petrovvich.webapp.storage.serialization.SerializationContext;
+
 public class ObjectStreamPathStorageTest extends AbstractStorageTest {
 
     public ObjectStreamPathStorageTest() {
-        super(new ObjectStreamPathStorage("C:\\Projects\\basejava\\storage") {
-        });
+        super(new ObjectStreamPathStorage("C:\\Projects\\basejava\\storage", new SerializationContext()));
     }
 }
